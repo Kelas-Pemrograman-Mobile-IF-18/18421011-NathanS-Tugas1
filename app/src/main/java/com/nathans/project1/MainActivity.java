7249d0e188791b63986999708a7865a4b06150cf
NathanS <nathan.18421011@student.ubl.ac.idgit config --global user.name nathansandhikagit remote add origin https://github.com/kelas-pemrograman-3-informatika18/kelas-pemrograman-3-informatika18-latihan1-nathansandhika-18421011.gitgit push -u origin master$ git config --global user.email nathan.18421011@student.ubl.ac.id>
2021-03-24 23:03:14 +0700

    first commit

---
+++ app/src/main/java/com/nathans/project1/MainActivity.java
@@ -0,0 +1,38 @@
+package com.nathans.project1;
+
+import android.content.Intent;
+import android.net.Uri;
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.app.Activity;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void cellphone(View view) {
+        Uri uri = Uri.parse("tel:08986735932");
+        Intent it = new Intent(Intent.ACTION_VIEW, uri);
+        startActivity(it);
+    }
+
+    public void email(View view) {
+        Intent intent = new Intent(Intent.ACTION_SEND);
+        intent.setType("text/plain");
+        intent.putExtra(Intent.EXTRA_EMAIL, new String[]{ "nnexus016@gmail.com"});
+        intent.putExtra(Intent.EXTRA_SUBJECT, "Biodata from Android Studio");
+        intent.putExtra(Intent.EXTRA_TEXT, "Hello from the other siiiiiiiidddeee!");
+
+        try {
+            startActivity(Intent.createChooser(intent, "Choose an Email :"));
+        } catch (android.content.ActivityNotFoundException ex) {
+        }
+    }
+
+}
